419fc31de3280e9f90f2391b0466a2d3f0c37a24
IamMohitGupta <72707911+IamMohitGupta@users.noreply.github.com>
2022-04-13 11:57:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9oaXRndXB0YTA2MDQwMiIsImEiOiJjbDF4NjlvczQwNmoxM2RxcWVjd3dpbXIxIn0.VV8qjjjsmzbdDUz7rtaaew';
+
+    navigator.geolocation.getCurrentPosition(sLocation,wLocation,{enableHighAccuracy:true})
+
+    function sLocation(position){
+        console.log(position);
+        setup([position.coords.longitude,position.coords.latitude]);
+    }
+
+    function wLocation(){
+
+    }
+
+    function setup(center){
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center:center,
+            // center: [-74.5, 40], // starting position [lng, lat]
+            zoom: 8 // starting zoom
+        });
+    }
+
+    
